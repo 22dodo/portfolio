c62b9c5d297bbfa437d9c0211edfe78d4865f6d0
22dodo <76244529+22dodo@users.noreply.github.com>
2022-06-06 11:08:33 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.route("/")
+
+.get(function(req,res){
+  res.render("home");
+});
+
+app.route("/:postName")
+
+.get(function(req,res){
+  const url = _.lowerCase(req.params.postName);
+  res.render(url);
+});
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
